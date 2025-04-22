2b7721827fc77ec02c9063c03995c442d7715e8d
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-04-22 07:02:10 +0300

    feat: initialize React app with Vite, add game components and styles
    - Added main application structure in App.tsx with player sections and input for goal.
    - Included dice images (dice-1.png to dice-6.png) for gameplay visuals.
    - Created flowchart image for game logic (pig-game-flowchart.png).
    - Integrated Vite as the build tool with configuration in vite.config.ts.
    - Set up TypeScript configuration files for app and node environments.
    - Added global styles in index.css, utilizing Tailwind CSS and custom styles.
    - Imported React and necessary dependencies in main.tsx for app rendering.
    - Included React logo SVG in assets for branding.

---
+++ react/src/App.tsx
@@ -0,0 +1,35 @@
+
+function App() {
+
+  return (
+    <main>
+      <div className="inpBox">
+        <input type="text" name="goal" id="goal" />
+        <label htmlFor="goal">Enter Goal . . .</label>
+      </div>
+      <section className="player player--0 player--active">
+        <h2 className="name" id="name--0">Player 1</h2>
+        <p className="score" id="score--0">43</p>
+        <div className="current">
+          <p className="current-label">Current</p>
+          <p className="current-score" id="current--0">0</p>
+        </div>
+      </section>
+      <section className="player player--1">
+        <h2 className="name" id="name--1">Player 2</h2>
+        <p className="score" id="score--1">24</p>
+        <div className="current">
+          <p className="current-label">Current</p>
+          <p className="current-score" id="current--1">0</p>
+        </div>
+      </section>
+
+      <img src="dice-5.png" alt="Playing dice" className="dice" />
+      <button className="btn btn--new">🔄 New game</button>
+      <button className="btn btn--roll">🎲 Roll dice</button>
+      <button className="btn btn--hold">📥 Hold</button>
+    </main>
+  )
+}
+
+export default App
